e55354e98248b8e7647094a13f1e785318676898
codesbyk <kiran.ray99@gmail.com>
2025-01-24 10:06:53 +0530

    Refactored error controller handled DB errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,8 +1,50 @@
-module.exports = (err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
+const AppError = require('./../utils/appError');
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = (err) => {
+  const message = `Duplicate field value ${err.keyValue.name}, Please use another value`;
+  return new AppError(message, 400);
+};
+
+const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
     message: err.message,
+    stack: err.stack,
+    error: err,
   });
+};
+
+const sendErrorProd = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    res.status(500).json({
+      status: 'Error',
+      message: 'Something went wrong. Internal error occurred',
+    });
+  }
+};
+
+module.exports = (err, req, res, next) => {
+  let convertErrorObj = JSON.parse(JSON.stringify(err));
+  let error = { ...convertErrorObj };
+  error.statusCode = error.statusCode || 500;
+  error.status = error.status || 'error';
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(error, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.errorResponse.code === 11000)
+      error = handleDuplicateFieldsDB(error);
+    sendErrorProd(error, res);
+  }
 };
